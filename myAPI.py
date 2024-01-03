6a259bcf599dc85e2055541417cc001d35103140
Bandhan Majumder <133476557+bandhan-majumder@users.noreply.github.com>
2024-01-03 22:18:46 +0530

    Update myAPI.py

--- myAPI.py
+++ myAPI.py
@@ -1,3 +1,3 @@
-my_news_api_key="689d3583d4c64174923a594c4ac308c8"
-my_weather_api_key="57d68e5b4893408c81af76cc2ae3f300"
-my_openAI_key="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"
+my_news_api_key="add_your_api_key"
+my_weather_api_key="add_your_api_key"
+my_openAI_key="add_your_api_key"
