88f9bf5c2e1255c2e6fb5d997ba800f32fc0a705
Bandhan Majumder <133476557+bandhan-majumder@users.noreply.github.com>
2024-01-03 22:43:56 +0530

    Update myAPI.py

--- myAPI.py
+++ myAPI.py
@@ -1,3 +1,3 @@
-my_news_api_key="add_your_api_key"
-my_weather_api_key="add_your_api_key"
-my_openAI_key="add_your_api_key"
+my_news_api_key="add_your_api_key" # https://newsapi.org/
+my_weather_api_key="add_your_api_key" # https://www.weatherbit.io/api
+my_openAI_key="add_your_api_key" # https://platform.openai.com/api-keys
